16ba4dd192565677c02b9650be7a8eaf43c10783
Allen Gooch <allen.gooch@gmail.com>
2017-09-24 15:27:17 -0500

    Perform preloads.

--- apps/fumepedia_web/lib/fumepedia_web/resolvers/line_resolver.ex
+++ apps/fumepedia_web/lib/fumepedia_web/resolvers/line_resolver.ex
@@ -1,14 +1,15 @@
 defmodule FumepediaWeb.Resolvers.LineResolver do
   @moduledoc false
 
+  import Ecto.Query
   alias Fumepedia.{Cigar, Repo}
 
   def all(_args, _info) do
-    {:ok, Repo.all(Cigar.Line)}
+    {:ok, (Repo.all from l in Cigar.Line, preload: [:vitolas])}
   end
 
   def find(%{id: id}, _info) do
-    case Repo.get(Cigar.Line, id) do
+    case (Repo.one from l in Cigar.Line, where: l.id == ^id, preload: [:vitolas]) do
       nil     -> {:error, "Line id #{id} not found"}
       line -> {:ok, line}
     end
